53e99343ce5d0fbae537e40cb3589d6bf9019edf
Nariman Ebuev <nariman101286@gmail.com>
2019-05-25 12:29:57 +0300

    css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
-
+import logo1 from './components/img/logo1.jpg';
+import burger from './components/img/burger.jpg';
 
 
 
 const App = () => {
     return (
-       <div className='app-wrapper'>
-          Заново
-       </div>
+        <div className='app-wrapper'>
+           
+            <header className='header'>
+            </header>
+            <nav className='nav'>
+                <img src={logo1} alt='logo' />
+                <a href='#'> Главная </a>
+                <a href='#'> Меню </a>
+                <a href='#'> Как заказать </a>
+                <a href='#'> Моё портфолио </a>
+                <a href='#'> О компании </a>
+            </nav>
+            <div className='content'>
+            <img src={burger} />
+            </div>
+        </div>
     );
 
 }
